f9b64fb0b2800e8ce142828adab4db7a767a286b
chrysa <chrysa@users.noreply.github.com>
2024-06-16 22:24:18 +0200

    fix bugs

--- common.py
+++ common.py
@@ -2,6 +2,7 @@ import csv
 from dataclasses import dataclass
 from dataclasses import field
 from pathlib import Path
+from typing import Any
 
 import ebookmeta
 
@@ -10,15 +11,12 @@ import ebookmeta
 class Common:
     epub_path: Path
     epub_list: list[Path] = field(default_factory=list)
-    duplicate_folder: Path = Path() / "[duplicates]"
-    failed_folder: Path = Path() / "[failed]"
     csv_exclude_header: list[str] = field(default_factory=list)
     csv_header: list[str] = field(default_factory=list)
     csv_output: Path = Path() / "epub_metadata.csv"
     data: list[dict] = field(default_factory=list)
     duplicates: list[tuple[Path, str]] = field(default_factory=list)
     identifier_list: list[str] = field(default_factory=list)
-    processed_folder: Path = Path() / "[processed]"
     progress_suffix: str = "[%(index)d/%(max)d] - [%(percent)d%%]"
 
     def __post_init__(self):
@@ -34,9 +32,10 @@ class Common:
         self.data = []
         self.duplicates = []
         self.identifier_list = []
-        self.duplicate_folder.mkdir(parents=True, exist_ok=True)
-        self.failed_folder.mkdir(parents=True, exist_ok=True)
-        self.processed_folder.mkdir(parents=True, exist_ok=True)
+        self.duplicate_folder: Path = Path() / self.args.duplicate_folder
+        self.failed_folder: Path = Path() /  self.args.failed_folder
+        self.processed_folder: Path = Path() /  self.args.processed_folder
+        self.skipped_folder: Path = Path() /  self.args.skipped_folder
 
     @staticmethod
     def _find_empty_folders():
@@ -67,11 +66,13 @@ class Common:
                 metadata = ebookmeta.get_metadata(epub.as_posix())
             except:
                 path = self.failed_folder / epub.name
+                self.failed_folder.mkdir(parents=True, exist_ok=True)
                 epub.rename(path)
                 is_failed = True
             else:
                 self.identifier_list.append(metadata.identifier)
                 is_failed = False
+                self.processed_folder.mkdir(parents=True, exist_ok=True)
                 path = self.processed_folder / epub.name
                 epub.rename(path)
             self.data.append(
@@ -113,12 +114,19 @@ class Common:
         epub.rename(author_folder / epub.name)
         self.update_data(identifier=metadata.identifier, path=author_folder / epub.name)
 
-    def rename_file(*, epub):
+    def rename_file(self,*, epub):
         metadata = ebookmeta.get_metadata(epub.as_posix())
-        epub.rename(
-            self.processed_folder
-            / f"{metadata.title.replace(',','_').replace(' ','_')}.{epub.suffix}",
-        )
+        if (self.processed_folder / f"{metadata.title.replace(',','_').replace(' ','_')}.{epub.suffix}").exists():
+            self.processed_skipped.mkdir(parents=True, exist_ok=True)
+            epub.rename(
+                self.processed_skipped / f"{metadata.title.replace(',','_').replace(' ','_')}.{epub.suffix}",
+            )
+        else:
+            self.processed_folder.mkdir(parents=True, exist_ok=True)
+            epub.rename(
+                self.processed_folder
+                / f"{metadata.title.replace(',','_').replace(' ','_')}.{epub.suffix}",
+            )
 
     def update_data(self, *, identifier, metadata=None, path=None):
         for data in self.data:
